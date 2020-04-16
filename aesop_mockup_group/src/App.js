b1c5c8941245e3f860edf09027a3f28fde138564
Visa <visalakshi.cse@gmail.com>
2020-04-16 17:40:03 -0500

    Two components added

--- aesop_mockup_group/src/App.js
+++ aesop_mockup_group/src/App.js
@@ -1,5 +1,6 @@
 import React,{Component} from 'react';
 import './App.css';
+import Secondpage from './components/2ndpage'
 
 import gift from "./images/Aesop-Gift-Kits-2019-20-Hybris-Range-Page-Secondary-Mid-Desktop-1690x945px.png";
 
@@ -12,19 +13,10 @@ super(props)
   {
   return (
     <div className="App">
-     <div id="giftdiv">
-      <p>
-     Five mythical Gift kifts
-    
-     <img src={gift} id="gift"/>
-     </p>
-        <pre>crafted for gods,mortals, and muses alike,our new Gift kit</pre>
-        <pre>collection makes for inspired gift giving this festive season</pre>
-        <input type="button" value="Explore seasonal Gift kits ->"/>
-       
+     
      
        
-      </div>
+    <Secondpage />
      
       
       
