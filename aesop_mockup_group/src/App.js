4b821665610d291c1fe814f0c820ca93187c3594
aparna4scholas <aparna4scholas@gmail.com>
2020-04-16 18:25:46 -0500

    added components

--- aesop_mockup_group/src/App.js
+++ aesop_mockup_group/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Fourthpage from './components/Fourthpage.js';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <Fourthpage />
     </div>
   );
 }
---
+++ aesop_mockup_group/src/components/Fourthpage.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+//import img4 from 'aesop_mockup/aesop_mockup_group/src/images/Aesop-Bleecker-Street-Carousel-1-Desktop-2560x1440px.jpg';
+import img4 from '../images/Aesop-Bleecker-Street-Carousel-1-Desktop-2560x1440px.jpg';
+
+function Fourthpage ()
+{
+    return(
+
+        <div> 
+             
+                <p> Architecture and design </p>
+                <h4>Spaces of distinction</h4>
+                
+            <img src = {img4} alt='Bleecker'/>
+        </div>
+        
+    )
+}
+
+
+export default Fourthpage;
