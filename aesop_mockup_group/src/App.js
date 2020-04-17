fd6a3a99affac41cde2a8c3fef003ec9a1fe4bf1
Visa <visalakshi.cse@gmail.com>
2020-04-16 21:16:17 -0500

    removed style in app.js

--- aesop_mockup_group/src/App.js
+++ aesop_mockup_group/src/App.js
@@ -1,10 +1,6 @@
 import React,{Component} from 'react';
 import './App.css';
-<<<<<<< HEAD
 import Secondpage from './components/2ndpage'
-=======
-import Page1 from './components/Page1';
->>>>>>> 2f484e594a6e9c7c625b23fdfae3ef3e153aca93
 
 import gift from "./images/Aesop-Gift-Kits-2019-20-Hybris-Range-Page-Secondary-Mid-Desktop-1690x945px.png";
 
@@ -18,16 +14,12 @@ super(props)
   return (
     <div className="App">
      
-<<<<<<< HEAD
      
        
     <Secondpage />
      
       
       
-=======
-      <Page1/>
->>>>>>> 2f484e594a6e9c7c625b23fdfae3ef3e153aca93
     </div>
   );
   }
