d70242bab15e6a0b400c3073d9b0adf0dd71f9ce
aparna4scholas <aparna4scholas@gmail.com>
2020-04-16 22:21:28 -0500

    added fourthpage css file

--- aesop_mockup_group/src/components/Fourthpage.js
+++ aesop_mockup_group/src/components/Fourthpage.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 //import img4 from 'aesop_mockup/aesop_mockup_group/src/images/Aesop-Bleecker-Street-Carousel-1-Desktop-2560x1440px.jpg';
 import img4 from '../images/Aesop-Bleecker-Street-Carousel-1-Desktop-2560x1440px.jpg';
+import './Fourthpage.css'
 
 function Fourthpage ()
 {
@@ -9,12 +10,12 @@ function Fourthpage ()
 
         <div> 
              
-                <p> Architecture and design </p>
+                <p className="p1"> Architecture and design </p>
                 <h4>Spaces of distinction</h4>
                 
-            <img src = {img4} alt='Bleecker'/>
+            <img className="i1" src = {img4} alt='Bleecker'/>
         </div>
-        
+
     )
 }
 
