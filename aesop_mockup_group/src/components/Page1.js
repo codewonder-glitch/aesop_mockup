5a0cb4ecfdff0aa2383f011bd1f118e14726150d
codewonder-glitch <61803366+codewonder-glitch@users.noreply.github.com>
2020-04-17 11:30:01 -0500

    Revert "Aesop aparna"

--- aesop_mockup_group/src/components/Page1.js
+++ aesop_mockup_group/src/components/Page1.js
@@ -5,17 +5,21 @@ import homePage from '../images/Aesop-Homepage-Banner.jpg';
 import deo from '../images/staple_products/Large-PNG-Aesop-Personal-Deodorant-50mL-large.png';
 import handBalm from '../images/staple_products/Aesop-Body-Resurrection-Aromatique-Hand-Balm-75mL-large.png';
 import handWash from '../images/staple_products/Aesop-Hand-Resurrection-Aromatique-Hand-Wash-500mL-large.png';
+// import {  BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 
 function Page1() {
+
+    //modal to display. Reference:https://youtu.be/10FNqoPpNbE
     const [modalIsOPen, setModalIsOpen] = useState(false);
         return (
             <React.Fragment>
+               
             <div className="firstRow">
             
               {/* <a href="#" alt="link to " onClick={() => setModalIsOpen(true)}> */}
               <button className="buttonModal" onClick={() => setModalIsOpen(true)}>
-              <h6>Complimentary standard shipping on all orders  +</h6>
+              <h5>Complimentary standard shipping on all orders  +</h5>
               </button>       
               <div>
                  {/* <Modal isOpen={true}> */}
@@ -32,16 +36,38 @@ function Page1() {
                      $35
                      <br/>
                      Order by 12PM ET for delivery on the next working days<hr/>
+                     <button onClick={()=> setModalIsOpen(false)}>Close</button>
                  </Modal> 
-              </div>
+              </div >
             </div>
-            <div>
-              <img className="imageBanner" src={homePage}  alt="Home Page" height="500px"/>
+            {/* <div  className="background">
+
+            </div> */}
+
+            {/* container which holds image and menu options */}
+            <div className="container">
+            <ul className="menuPosition">
+                    <li>Shop</li>
+                    <li>Read</li>
+                    <li>Stores</li>
+                    <li>Search</li>
+                    <li style={{float:"right"}}>Login</li>
+                </ul> 
+                <h3 id="aesop">Aesop</h3> 
+                <p id="formualationsText">
+                    <h3>Formulations for skin, hair, home and body</h3>
+                <em>Create with sensory pleasure in mind</em></p>
+    
+             {/* used z-index to make image as background */}
+            <img className="imageBanner" src={homePage}  alt="Home Page" height="500px" width='100%'/>
+               
+             
             </div>        
            
+            {/* created as flex */}
             <div className="leftSideDiv">
                 <div>
-                    <h6>Aesop essentials</h6>
+                    <h5>Aesop essentials</h5>
                     <br/>
                    
                     <h2> A selection of staples</h2>
@@ -51,26 +77,29 @@ function Page1() {
                     agao to personal care necessites
                 </p>
                 </div>
+                 {/* created as flex */}
                 <div className="rightSideDiv">
                 <div>
-                    <img src={handWash} alt="hand wash"  height="200px" /> 
+                    <img src={handWash} alt="hand wash"  height="400px" /> 
                     <h6>Resurrection Aromatique Hand Wash</h6>
                     <h6><em>Gentle cleansing for hard-working hands</em></h6>
                 </div>
 
                 <div>
-                    <img src={handBalm} alt="hand balm"  height="200px" />
+                    <img src={handBalm} alt="hand balm"  height="400px" />
                     <h6>Resurrection Aromatique Hand Wash</h6>
                     <h6><em>Richly hydrating, greaseless finish</em></h6>
                 </div>
                 <div>
-                    <img src={deo} alt="deodarant"  height="200px" />   
+                    <img src={deo} alt="deodarant"  height="400px" />   
                     <h6>Deodarant </h6>
                     <h6><em>Woody, herbaceous, earthy aromatic</em></h6>
                 </div>
-
+                
                 </div>
+               
             </div>
+            <hr color="lightgray"/>
             </React.Fragment>
         );
     }
